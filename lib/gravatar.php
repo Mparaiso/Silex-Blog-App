be3dc64386e260e1ee0aeebf2d01473533e98ada
mparaiso <aikah@free.fr>
2012-09-06 23:45:22 +0200

    silex blog commit 6-septembre-2012:11-45

--- lib/gravatar.php
+++ lib/gravatar.php
@@ -1,20 +1,21 @@
 <?php
-namespace Lib;
+namespace Lib{
 
-class Gravatar{
-  /**
-   *
-   * @var string
-   */
-  const URL = "http://www.gravatar.com/avatar/";
-
-  /**
-   * returns a gravatar url
-   * @param string $email
-   * @param string $size
-   * @return string
-   */
-  function getGravatar($email,$size=50){
-    return self::URL."?gravatar_id=".md5(strtolower($email))."&size=$size";
+  class Gravatar{
+    /**
+     *
+     * @var string
+     */
+    const URL = "http://www.gravatar.com/avatar/";
+    /**
+     * returns a gravatar url
+     * @param string $email
+     * @param string $size
+     * @return string
+     */
+    function getGravatar($email,$size=50){
+      return self::URL."?gravatar_id=".md5(strtolower($email))."&size=$size";
+    }
   }
+
 }
